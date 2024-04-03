602b6cf6f39755dd49703b0cf1f6c497ddd17f4d
iamkani <arunkaniyamattam@gmail.com>
2024-04-03 12:47:21 -0300

    Update JSoupFuzzer.java

--- JSoupFuzzer.java
+++ JSoupFuzzer.java
@@ -1,7 +1,7 @@
 // Copyright 2021 Patrick Ventuzelo
 // Copyright 2021 Fuzzing Labs
 // 
-// Website: https://fuzzinglabs.com/test123456
+// Website: https://fuzzinglabs.com/test123456789
 
 import org.jsoup.Jsoup;
 import com.code_intelligence.jazzer.api.FuzzedDataProvider;
